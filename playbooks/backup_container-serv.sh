3f7e02312294bc08733a42807243fad6c17241f6
opercoder <83242745+opercoder@users.noreply.github.com>
2022-02-04 11:48:22 +0300

    Update backup_container-serv.sh

--- playbooks/backup_container-serv.sh
+++ playbooks/backup_container-serv.sh
@@ -3,7 +3,5 @@ dirBack="/home/user/backup/data/data"
 if [ ! -d "$dirVol" ]; then mkdir -p $dirVol
 fi
 cp -r /data $dirBack
-
 tar -czf "/home/user/backup/data/container-server_data_$(date '+%Y-%m-%d').tar.gz" $dirBack
-
 rm -r $dirBack
